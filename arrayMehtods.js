00ebbf34641bc403c6d149688214f617758d36d0
Alvison Hunter Arnuero <alvison@gmail.com>
2020-11-19 00:57:09 -0600

    Array methods for demo clip

---
+++ arrayMehtods.js
@@ -0,0 +1,14 @@
+//Basic Array Methods in JavaScript
+//Made with ❤️ by Alvison Hunter
+// November 10th, 2020
+let arrPeople = ['👶','👨','👩','👳','🎅'];
+//Function to call the array methods
+const tryArrayMethods =()=>{
+  console.log(arrPeople)
+  let res = arrPeople.push('👨‍')
+  console.log(res)
+  console.log('New Array:', arrPeople)
+}
+// Call the main function
+tryArrayMethods()
+// '🥰'  🧔 👨‍🦰 👨‍🦱 👨‍🦳
